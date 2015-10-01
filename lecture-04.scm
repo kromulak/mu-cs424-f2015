ca8d08934207047bf4381910b2c583fd8c8e16e7
Barak A. Pearlmutter <barak+git@pearlmutter.net>
2015-10-01 14:54:01 +0100

    table driven derivative taking

--- lecture-04.scm
+++ lecture-04.scm
@@ -3,20 +3,28 @@
 ;;;  - code is ugly
 
 (define d
-   (λ (e x)
-     (cond ((number? e) 0)
-	   ((equal? e x) 1)
-	   ((symbol? e) 0)
-	   (else
-	    (let ((u (cadr e))
-		  (v (caddr e)))
-	      (cond ((equal? (car e) '+)
-		     (lle+ (d u x) (d v x)))
-		    ((equal? (car e) '*)
-		     ;; Leibnitz Rule: d/dx u*v = u * dv/dx + du/dx * v
-		     (lle+ (lle* u (d v x))
-			   (lle* (d u x) v)))
-		    (else (error "bad LLE" le))))))))
+  (λ (e x)
+    (cond ((number? e) 0)
+	  ((equal? e x) 1)
+	  ((symbol? e) 0)
+	  ((pair? e)
+	   (let ((f (car e)))
+	     (apply (lookup f dtable)
+		    (append (cdr e) (map (λ (u) (d u x)) (cdr e))))))
+	  (else (error "bad LLE" le)))))
+
+(define dtable
+  (list (list '+ (λ (u v du dv) (lle+ du dv)))
+	;; Leibnitz Rule: d(u*v) = u*dv + du*v
+	(list '* (λ (u v du dv) (lle+ (lle* u dv) (lle* du v))))
+	(list 'sin (λ (u du) (lle* (lle-cos u) du)))
+	(list 'cos (λ (u du) (lle* (lle* -1 (lle-sin u)) du)))))
+
+(define lookup
+  (λ (x alist)
+    (if (equal? x (caar alist))
+	(cadar alist)
+	(lookup x (cdr alist)))))
 
 (define lle+
   (λ (e1 e2)
@@ -35,6 +43,16 @@
 	  ((equal? e1 1) e2)
 	  ((equal? e2 1) e1)
 	  (else (list '* e1 e2)))))
+
+(define lle-sin
+  (λ (e)
+    (cond ((number? e) (sin e))
+	  (else (list 'sin e)))))
+
+(define lle-cos
+  (λ (e)
+    (cond ((number? e) (cos e))
+	  (else (list 'cos e)))))
 
 ;;; aside: scheme tries to be regular & general.
 ;;; e.g., identity elements
