ee6c118d5e7db10ac59237bf319c376cd083e378
Barak A. Pearlmutter <barak+git@pearlmutter.net>
2015-10-08 14:34:12 +0100

    WRONG: my-apply needs to *bind* vars to args ...

--- lecture-06.scm
+++ lecture-06.scm
@@ -6,9 +6,32 @@
 
 (define my-eval
   (λ (e)
-    (cond ((number? e) e)
-	  ((equal? (car e) '+)
-	   (+ (my-eval (cadr e)) (my-eval (caddr e))))
-	  ((equal? (car e) '*)
-	   (* (my-eval (cadr e)) (my-eval (caddr e))))
+    (cond ((or (number? e) (boolean? e)) e)
+	  ((symbol? e) (lookup e global-env))
+	  ((pair? e)
+	   (let ((f (car e)))
+	     (cond ((equal? f 'λ)
+		    (let ((vars (cadr e))
+			  (body (caddr e)))
+		      (list 'closure
+			    vars
+			    body)))
+		   ((equal? f 'let) xxx)
+		   (else
+		    ;; regular function call
+		    (my-apply (my-eval f)
+			      (map my-eval (cdr e)))))))
 	  (else (error "bad expression" e)))))
+
+(define my-apply
+  (λ (p args)
+    (cond ((procedure? p) (apply p args))
+	  ((and (pair? p) (equal? (car p) 'closure))
+	   (my-eval (caddr p)))
+	  (else (error "xxx")))))
+
+(define lookup (λ (k alist) (cadr (assoc k alist))))
+
+(define global-env (list (list '+ (λ (x y) (+ x y)))
+			 (list '* (λ (x y) (* x y)))
+			 (list 'sin sin)))
