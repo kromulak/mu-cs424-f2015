f035006c0ed3f1335b3cbe4872ad39010da42e97
Barak A. Pearlmutter <barak+git@pearlmutter.net>
2015-10-08 14:44:36 +0100

    working except for free vars
    (my-eval '(((λ (x) (λ (y) (+ x y))) 1) 22) '())
    dies because it treats x as a global variable instead of knowing its
    value at the site the function was defined in

--- lecture-06.scm
+++ lecture-06.scm
@@ -5,12 +5,12 @@
 ;; 3
 
 (define my-eval
-  (λ (e)
+  (λ (e env)
     (cond ((or (number? e) (boolean? e)) e)
-	  ((symbol? e) (lookup e global-env))
+	  ((symbol? e) (lookup e (append env global-env)))
 	  ((pair? e)
 	   (let ((f (car e)))
-	     (cond ((equal? f 'λ)
+	     (cond ((or (equal? f 'λ) (equal? f 'lambda))
 		    (let ((vars (cadr e))
 			  (body (caddr e)))
 		      (list 'closure
@@ -19,15 +19,16 @@
 		   ((equal? f 'let) xxx)
 		   (else
 		    ;; regular function call
-		    (my-apply (my-eval f)
-			      (map my-eval (cdr e)))))))
+		    (my-apply (my-eval f env)
+			      (map (λ (a) (my-eval a env))
+				   (cdr e)))))))
 	  (else (error "bad expression" e)))))
 
 (define my-apply
   (λ (p args)
     (cond ((procedure? p) (apply p args))
 	  ((and (pair? p) (equal? (car p) 'closure))
-	   (my-eval (caddr p)))
+	   (my-eval (caddr p) (map list (cadr p) args)))
 	  (else (error "xxx")))))
 
 (define lookup (λ (k alist) (cadr (assoc k alist))))
