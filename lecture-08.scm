33f71d4d74ecd3515d3a6a5c945a43c9ffb9852b
Barak A. Pearlmutter <barak+git@pearlmutter.net>
2015-10-15 14:56:32 +0100

    CPS

---
+++ lecture-08.scm
@@ -0,0 +1,97 @@
+;;; Continuation Passing Style
+;;;  = CPS
+
+;;; Properties of CPS
+;;; - rewriting code, s.t.,
+;;; - no anonymous temporary value (all temps are named)
+;;;   No can write: a+b*c aka (+ a (* b c))
+;;;   because b*c needs a name.
+;;; - no non-tail-recursive procedure calls
+;;; - never return a value (can only call continuation on it)
+;;; - turns code inside-out and makes it all confusing
+
+;;; Add extra paramter to all procedures called the "continuation"
+
+(define c+ (λ (c x y) (c (+ x y))))
+(define c* (λ (c x y) (c (* x y))))
+(define c- (λ (c x y) (c (- x y))))
+(define c= (λ (ct cf x y) ((if (= x y) ct cf))))
+
+;;; Non-TR-definition of factorial
+(define fact
+  (λ (n)
+    (if (= n 0)
+	1
+	(* n (fact (- n 1))))))
+
+
+;;; TR-definition of factorial
+(define fact-tr
+  (λ (n)
+    (fact-aux 1 n)))
+
+(define fact-aux
+  (λ (a n)
+    (if (= n 0)
+	a
+	(fact-aux (* a n) (- n 1)))))
+
+;;; Convert to CPS
+
+(define cfact
+  (λ (c n)
+    (c= (λ () (c 1))
+	(λ () (c- (λ (nm1)
+		    (cfact (λ (fnm1)
+			     (c* c n fnm1))
+			   nm1))
+		  n 1))
+	n 0)))
+
+(define cfact-tr
+  (λ (c n)
+    (cfact-aux c 1 n)))
+
+(define cfact-aux
+  (λ (c a n)
+    (c= (λ () (c a))
+	(λ ()
+	  (c* (λ (atn)
+		(c- (λ (nm1)
+		      (cfact-aux c atn nm1))
+		    n 1))
+	      a n))
+	1 n)))
+
+;; > (cfact (λ (r) (list r r r)) 3)
+;; (6 6 6)
+
+(define id (λ (x) x))			; identity function
+
+;; > (cfact id 100)
+;; 93326...
+
+(define cfact-aux
+  (λ:cfact_aux (c a n)
+    (c= (λ:L1 () (c a))
+	(λ:L2 ()
+	  (c* (λ:L3 (atn)
+		(c- (λ:L4 (nm1)
+		      (cfact-aux c atn nm1))
+		    n 1))
+	      a n))
+	1 n)))
+
+;; cfact_aux:
+;; 	push 1
+;; 	push n
+;; 	= L2
+;; L1:	push a
+;; 	ret
+;; L2:
+;; 	push a
+;; 	push n
+;; 	TIMES
+;; 	...
+;; 	SUBTRACT
+;; 	...
