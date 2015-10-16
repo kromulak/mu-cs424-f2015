a2e2f98bfe1de5b9b4238ec5b76f510ec06cd115
Barak A. Pearlmutter <barak+git@pearlmutter.net>
2015-10-16 16:53:02 +0100

    typos and load-o

--- lecture-08.scm
+++ lecture-08.scm
@@ -39,27 +39,27 @@
 
 (define cfact
   (λ (c n)
-			       (c* c n fnm1))
-			nm1))
-	      n 1))
     (c= (λ () (c 1))
-	(λ () (c- (λ (nm1) ;; nm1 = n-1
+	(λ () (c- (λ (nm1)	     ;; nm1 = n-1
 		    (cfact (λ (fnm1) ;; fnm1 = (n-1)!
+			     (c* c n fnm1))
+			   nm1))
+		  n 1))
 	n 0)))
 
 (define cfact-tr
   (λ (c n)
     (cfact-aux c 1 n)))
 
-(define cfact-aux
-  (λ:cfact_aux (c a n) ; return address
-    (c= (λ:L1 () (c a))
-        (λ:L2 ()
-	  (c* (λ:L3 (atn)
-		(c- (λ:L4 (nm1)
-		      (cfact-aux C atn nm1)) n 1))
-	      a n))
-        1 n)))
+'(define cfact-aux
+   (λ:cfact_aux (c a n) ; return address
+     (c= (λ:L1 () (c a))
+	 (λ:L2 ()
+	   (c* (λ:L3 (atn)
+		 (c- (λ:L4 (nm1)
+		       (cfact-aux C atn nm1)) n 1))
+	       a n))
+	 1 n)))
 
 ;;cfact_aux:
 ;;	push 1
