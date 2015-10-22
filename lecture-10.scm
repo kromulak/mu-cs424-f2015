d79b3393d2fa2b839b018c7e894d801991477249
Barak A. Pearlmutter <barak+git@pearlmutter.net>
2015-10-22 14:57:02 +0100

    working up to fixedpoint combinator

---
+++ lecture-10.scm
@@ -0,0 +1,116 @@
+;;; Migration of semantic constructs to "user level"
+;;; - local variable binding (macro expand let into ((λ ...)...))
+;;; - conditionals like if (macro expand into closures & library function or λ-expressions)
+;;; - goto, loops (tail-recursive procedure call)
+;;; - non-tail-recursive procedure call (use continuations; convert to CPS)
+;;; - data structures (implement as closures) [L09]
+;;; - objects (closures) [L09]
+
+(define count-nums
+  (λ (s)
+    (cond ((number? s) 1)
+	  ((pair? s) (+ (count-nums (car s))
+			(count-nums (cdr s))))
+	  (else 0))))
+
+;;; could pass the following expression to my-eval ... but it will get an error.
+
+'(let ((count-ns (λ (s)
+		   (cond ((number? s) 1)
+			 ((pair? s) (+ (count-ns (car s))
+				       (count-ns (cdr s))))
+			 (else 0)))))
+   (count-ns '(aye (((((bee (((3) dee))))) 8)))))
+
+;; > (let ((pi (+ pi 1))) pi)
+;; 4.141592653589793
+;; > (let ((x (+ x 1))) x)
+;; x: undefined;
+;;  cannot reference undefined identifier
+;;   context...:
+;;    /usr/share/racket/collects/racket/private/misc.rkt:87:7
+
+(let ((count-nums0 (λ (count-nums00 s)
+		     (cond ((number? s) 1)
+			   ((pair? s) (+ (count-nums00 count-nums00 (car s))
+					 (count-nums00 count-nums00 (cdr s))))
+			   (else 0)))))
+
+  (let ((count-nums (λ (s)
+		      (count-nums0 count-nums0 s))))
+
+    (count-nums '(aye (((((bee (((3) dee))))) 8))))))
+
+;;; Curried Function
+
+(define regular-plus (λ (x y) (+ x y)))
+(define curried-plus (λ (x) (λ (y) (+ x y))))
+
+(define curry (λ (non-curried-binary-function) (λ (x) (λ (y) (non-curried-binary-function x y)))))
+(define uncurry (λ (curried-binary-function) (λ (x y) ((curried-binary-function x) y))))
+
+;; > (curry +)
+;; #<procedure:...4/lecture-10.scm:49:47>
+;; > ((curry +) 1)
+;; #<procedure:...4/lecture-10.scm:49:54>
+;; > (((curry +) 1) 17)
+;; 18
+;; > ((uncurry (curry +)) 3 4)
+;; 7
+
+;;; Convert above to curried form:
+
+(let ((count-nums0 (λ (count-nums00)
+		     (λ (s)
+		       (cond ((number? s) 1)
+			     ((pair? s) (+ ((count-nums00 count-nums00) (car s))
+					   ((count-nums00 count-nums00) (cdr s))))
+			     (else 0))))))
+
+  (let ((count-nums (λ (s)
+		      ((count-nums0 count-nums0) s))))
+
+    (count-nums '(aye (((((bee (((3) dee))))) 8))))))
+
+;;; η-reduction ("eta reduction")
+
+;; this: (λ (x) (e x))
+;; is equivalent to this: e
+
+;; eta-convert the above:
+
+(let ((count-nums0 (λ (count-nums00)
+		     (λ (s)
+		       (cond ((number? s) 1)
+			     ((pair? s) (+ ((count-nums00 count-nums00) (car s))
+					   ((count-nums00 count-nums00) (cdr s))))
+			     (else 0))))))
+
+  (let ((count-nums (count-nums0 count-nums0)))
+
+    (count-nums '(aye (((((bee (((3) dee))))) 8))))))
+
+;;; Try to simplify ...
+
+(let ((count-nums0 (λ (count-nums00)
+		     (let ((f (count-nums00 count-nums00)))
+		       (λ (s)
+			 (cond ((number? s) 1)
+			       ((pair? s) (+ (f (car s))
+					     (f (cdr s))))
+			       (else 0)))))))
+
+  (let ((count-nums count-nums0))
+
+    (count-nums '(aye (((((bee (((3) dee))))) 8))))))
+
+;; g = (λ (f) (λ (s) (... code that tries to recurse by calling f ...)))
+;; want to convert g into recursive function.
+;; let's call (Y g) that recursive function.
+;; Want the following property: (g (Y g)) = (Y g)
+;; In math, when   f(c) = c   we say that c is fixedpoint of f.
+;; We want to find a fixedpoint of g.
+;; We want (Y g) to be a fixedpoint of g.
+
+;; (define Y (λ (g) (g (Y g)))) ; won't work because ...
+;; (define Y (λ (g) (g (g (g (Y g)))))) ; ick ...
