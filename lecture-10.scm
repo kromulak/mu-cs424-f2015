a33591ee14f47a9c249a68ae2ede7b10edea1280
Barak A. Pearlmutter <barak+git@pearlmutter.net>
2015-10-27 16:50:44 +0000

    typo

--- lecture-10.scm
+++ lecture-10.scm
@@ -1,6 +1,6 @@
 ;;; Migration of semantic constructs to "user level"
 ;;; - local variable binding (macro expand let into ((λ...)...))
-;;; - conditionals like if (macro expand into clopsures & library function or lambda/λ-expressions)
+;;; - conditionals like if (macro expand into closures & library function or lambda/λ-expressions)
 ;;; - goto, loops (tail-recursive procedure call)
 ;;; - non-tail-recursive procedure call (use continuations; convert to CPS)
 ;;; - data structures (implement as closures) [L09]
