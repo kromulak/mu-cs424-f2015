38982b300084c63f8a1870cea29f0ba8a4a088c0
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-11-15 12:42:43 +0000

    spelling and spacing

--- lecture-13.scm
+++ lecture-13.scm
@@ -1,13 +1,13 @@
-;;;Introduction to Lambda Calculus
+;;; Introduction to Lambda Calculus
 
-;;;Slides can be found here: http://www-verimag.imag.fr/~iosif/LogicAutomata07/lambda-calculus-slides.pdf
+;;; Slides can be found here: http://www-verimag.imag.fr/~iosif/LogicAutomata07/lambda-calculus-slides.pdf
 ;;; Covers: 
-;;; Synatic sugar in lambda calculus
+;;; Syntactic sugar in lambda calculus
 ;;; α-conversion/α-renaming
 ;;; scoping & parameter passing
 ;;; β-reduction
 ;;; -> note that the way of writing β-reduction in the slides is the standard way, but not the way Barak writes it in class.
-;;;The Church-Roser Theorem -> if we apply x and y to a to get b and c, we can apply y to a and x to b and both will equal d.
+;;; The Church-Rosser Theorem -> if we apply x and y to a to get b and c, we can apply y to a and x to b and both will equal d.
 ;;;
 
 
@@ -18,7 +18,7 @@
 (define two   (λ (f) (λ (x) (f (f x)))))
 (define three (λ (f) (λ (x) (f (f (f x))))))
 
-;;;"succ" named "inc" in lecture12 notes
+;;; “succ" named "inc" in lecture12 notes
 (define succ  (λ (n) (λ (f) (λ (x) (f ((n f) x)))))) ; traditional name: successor
 
 (define plus1 (λ (x) (+ x 1)))
@@ -28,7 +28,7 @@
 (define true   (λ (x) (λ (y) x)))
 (define false  (λ (x) (λ (y) y)))
 
-;;;encoding pairs in Lambda Calculus is similar to cons in scheme
+;;; encoding pairs in Lambda Calculus is similar to cons in scheme
 (define pair (λ (x) (λ (y) (λ (b) ((b x) y)))))
 (define fst  (λ (p) (p true)))
 (define snd  (λ (p) (p false)))
@@ -38,16 +38,16 @@
 ;; > (church-to-int (snd ((pair two) three)))
 ;; 3
 
-;;;I is the identity combinator
+;;; I is the identity combinator
 (define I      (λ (x) x))
-;;;K is the constant combinator
+;;; K is the constant combinator
 (define K      (λ (x) (λ (y) x)))
 
-;;;note that as an alternative to Lambda Calculus, we can use combinator logic/SKI-calculus, which only uses the cambinators I, K and S
-;;;Combinator Logic is also Turning complete.
+;;; note that as an alternative to Lambda Calculus, we can use combinator logic/SKI-calculus, which only uses the combinators I, K and S
+;;; Combinator Logic is also Turing complete.
 
 (define iszero (λ (n) ((n (K false)) true)))
-;;;When checking if a number is zero, if we apply the number to iszero 0 times, we get true, apply it more times and false is returned.
+;;; When checking if a number is zero, if we apply the number to iszero 0 times, we get true, apply it more times and false is returned.
 
 (define church-to-bool (λ (b) ((b #t) #f)))
 
@@ -64,8 +64,8 @@
 ;;                                (pair false (f (snd p))))
 ;;                      (pair true x))
 
-;;;We defined successor and now we want predecessor.
-;;;Apply the function n-1 times to return the predecessor. 
+;;; We defined successor and now we want predecessor.
+;;; Apply the function n-1 times to return the predecessor. 
 
 (define pred				; predecessor
   (λ (n)
@@ -87,8 +87,8 @@
 ;; > (church-to-int (pred zero))
 ;; 0
 
-;;;Now that we have defined pred, we can subtract. 
-;;;To subtract m from n, we need to apply pred to n, m times.
+;;; Now that we have defined pred, we can subtract. 
+;;; To subtract m from n, we need to apply pred to n, m times.
 (define subtract (λ (n) (λ (m) ((m pred) n))))
 
 ;; > (church-to-int ((subtract three) two))
@@ -103,4 +103,3 @@
 
 ;; Y f = f (Y f)
 ;; Y = λ f . (λ x . f (x x)) (λ x . f (x x))
-
