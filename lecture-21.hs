fcd4dcd8a2a0882782667fd7fa7e4c6e929cc391
ciaranc94 <ciarancumiskey18594@gmail.com>
2015-12-10 14:56:40 +0000

    Added lecture 22.

--- lecture-21.hs
+++ lecture-21.hs
@@ -1,1 +1,48 @@
 -- Note Haskell/* subdir
+
+{-
+
+putChar :: Char -> World -> (World, ())
+getChar :: World -> (World, Char)
+
+data IO a = World -> (World, a)
+
+putChar :: Char -> IO ()
+getChar :: IO Char
+
+ioChain :: IO a -> (a -> IO b) -> IO b
+
+semicolon :: IO a -> IO b -> IO b
+semicolon ioa iob = ioChain ioa (const iob)
+
+return a :: IO a
+	(does nothing, yields arg)
+
+-}
+
+-- ioChain = (>>=) infix operator
+-- semicolon = (>>)
+
+putString :: [Char] -> IO ()
+putString cs = concatWithSemi (map putChar cs)
+
+concatWithSemi :: [IO ()] -> IO ()
+concatWithSemi [] = return ()
+concatWithSemi [x] = x
+concatWithSemi (x:xs) = x >> concatWithSemi xs
+
+-------------------------------
+
+-- These are "Monads":
+
+-- Mebe a
+-- IO a
+-- Dist a
+
+-- *Main> :t return
+-- return :: Monad m => a -> m a
+
+-- "bind",
+
+-- *Main> :t (>>=)
+-- (>>=) :: Monad m => m a -> (a -> m)
